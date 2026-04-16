7f71f72ac3850020b542362c33370ae69f5dc128
Mertcan KIRCI <mertkirci02@gmail.com>
2026-04-16 19:19:58 +0300

    initalization

---
+++ kge-macos/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  kge-macos
+//
+//  Created by Mertcan Kırcı on 16.04.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ kge-macos/kge_macosApp.swift
@@ -0,0 +1,17 @@
+//
+//  kge_macosApp.swift
+//  kge-macos
+//
+//  Created by Mertcan Kırcı on 16.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct kge_macosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
