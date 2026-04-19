7f5db85f7ad85fddb35d556caf281c83d0f0d9d5
Mertcan KIRCI <mertkirci02@gmail.com>
2026-04-19 18:23:53 +0300

    Added fetch events

--- CalendarKit/Package.swift
+++ CalendarKit/Package.swift
@@ -13,11 +13,17 @@ let package = Package(
             targets: ["CalendarKit"]
         ),
     ],
+    dependencies: [
+        .package(path: "../Extensions"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "CalendarKit"
+            name: "CalendarKit",
+            dependencies: [
+                .product(name: "Extensions", package: "Extensions"),
+            ]
         ),
 
     ]
--- CalendarKit/Sources/CalendarKit/CalendarKit.swift
+++ CalendarKit/Sources/CalendarKit/CalendarKit.swift
@@ -2,63 +2,46 @@
 // https://docs.swift.org/swift-book
 
 import EventKit
+import Extensions
 
 public protocol CalendarInterface {
-    func createEvent(
-        title: String,
-        startDate: Date,
-        endDate: Date,
-        recurrenceFrequency: EKRecurrenceFrequency?,
-        recurrenceInterval: Int?,
-        recurrenceEnd: EKRecurrenceEnd?
-    ) throws
+    func createEvent(_ event: KGEEvent) throws
     
-    func deleteEvent(event: EKEvent) throws
+    func deleteEvent(_ event: KGEEvent) throws
+    
+    func fetchEvents() -> [KGEEvent]
 }
 
 public struct CalendarManager: CalendarInterface {
     private let eventStore = EKEventStore()
     
-    public func createEvent(
-        title: String,
-        startDate: Date,
-        endDate: Date,
-        recurrenceFrequency: EKRecurrenceFrequency?,
-        recurrenceInterval: Int?,
-        recurrenceEnd: EKRecurrenceEnd?
-    ) throws {
-        var rule: EKRecurrenceRule?
-        
-        if let recurrenceFrequency,
-           let recurrenceInterval {
-            rule = .init(
-                recurrenceWith: recurrenceFrequency,
-                interval: recurrenceInterval,
-                end: recurrenceEnd
-            )
-        }
-        
-        let event = EKEvent(eventStore: eventStore)
-        event.title = title
-        event.startDate = startDate
-        event.calendar = eventStore.defaultCalendarForNewEvents
-        
-        if let rule {
-            event.addRecurrenceRule(rule)
-        }
-        
+    public func createEvent(_ event: KGEEvent) throws {
         do {
-            try eventStore.save(event, span: .futureEvents)
+            let appleEvent = event.toEKEvent()
+            try eventStore.save(appleEvent, span: .futureEvents)
         } catch  {
             throw error
         }
     }
     
-    public func deleteEvent(event: EKEvent) throws {
+    public func deleteEvent(_ event: KGEEvent) throws {
         do {
-            try eventStore.remove(event, span: .futureEvents)
+            let appleEvent = event.toEKEvent()
+            try eventStore.remove(appleEvent, span: .futureEvents)
         } catch {
             throw error
         }
+    }
+    
+    public func fetchEvents() -> [KGEEvent] {
+        let calendars = eventStore.calendars(for: .event)
+        let endDate = Date()
+        let startDate = Calendar.current.date(byAdding: .month, value: -1, to: endDate) ?? Date()
+        let predicate = eventStore.predicateForEvents(withStart: startDate, end: endDate, calendars: calendars)
+        let allEvents = eventStore.events(matching: predicate)
+        let kgeEvents = allEvents
+            .filter({ $0.title.hasPrefix("#KGE - ") })
+            .map({ $0.toKGEEvent() })
+        return kgeEvents
     }
 }
