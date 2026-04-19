7c73e1640e04a38419b12f98becd32cb61c1a0ff
Mertcan KIRCI <mertkirci02@gmail.com>
2026-04-19 13:36:30 +0300

    re-added permissionkit calendar permission manager

--- PermissionKit/Sources/PermissionKit/CalendarPermissionManager.swift
+++ PermissionKit/Sources/PermissionKit/CalendarPermissionManager.swift
@@ -5,4 +5,36 @@
 //  Created by Mertcan Kırcı on 19.04.2026.
 //
 
-import Foundation
+import EventKit
+
+public final class CalendarPermissionManager: PermissionInterface {
+    private let eventStore = EKEventStore()
+    
+    var status: PermissionStatus {
+        let eventStatus = EKEventStore.authorizationStatus(for: .event)
+        
+        switch eventStatus {
+        case .fullAccess, .writeOnly, .authorized:
+            return .authorized
+        case .notDetermined:
+            return .notDetermined
+        case .restricted:
+            return .restricted
+        case .denied:
+            return .denied
+        @unknown default:
+            return .denied
+        }
+    }
+    
+    @discardableResult
+    func request() async throws -> PermissionStatus {
+        guard status == .notDetermined else { return status }
+        do {
+            let granted = try await eventStore.requestFullAccessToEvents()
+            return granted ? .authorized : .denied
+        } catch {
+            throw error
+        }
+    }
+}
--- PermissionKit/Sources/PermissionKit/PermissionStatus.swift
+++ PermissionKit/Sources/PermissionKit/PermissionStatus.swift
@@ -1,8 +1,16 @@
 //
-//  File.swift
+//  PermissionStatus.swift
 //  PermissionKit
 //
 //  Created by Mertcan Kırcı on 19.04.2026.
 //
 
 import Foundation
+
+public enum PermissionStatus {
+    case notDetermined
+    case authorized
+    case denied
+    case restricted
+    case limited 
+}
