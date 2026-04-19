08a7c8668c20c084ecd8c0a14dd18225a7c45b95
Mertcan KIRCI <mertkirci02@gmail.com>
2026-04-19 18:29:19 +0300

    small feature for calendarkit

--- CalendarKit/Sources/CalendarKit/CalendarKit.swift
+++ CalendarKit/Sources/CalendarKit/CalendarKit.swift
@@ -13,7 +13,11 @@ public protocol CalendarInterface {
 }
 
 public struct CalendarManager: CalendarInterface {
-    private let eventStore = EKEventStore()
+    private let eventStore: EKEventStore
+    
+    init(eventStore: EKEventStore = EKEventStore()) {
+        self.eventStore = eventStore
+    }
     
     public func createEvent(_ event: KGEEvent) throws {
         do {
@@ -35,13 +39,19 @@ public struct CalendarManager: CalendarInterface {
     
     public func fetchEvents() -> [KGEEvent] {
         let calendars = eventStore.calendars(for: .event)
-        let endDate = Date()
-        let startDate = Calendar.current.date(byAdding: .month, value: -1, to: endDate) ?? Date()
+        let (endDate, startDate) = getMonthlyDateSpan()
         let predicate = eventStore.predicateForEvents(withStart: startDate, end: endDate, calendars: calendars)
         let allEvents = eventStore.events(matching: predicate)
         let kgeEvents = allEvents
             .filter({ $0.title.hasPrefix("#KGE - ") })
             .map({ $0.toKGEEvent() })
         return kgeEvents
+    }
+    
+    private func getMonthlyDateSpan() -> (Date, Date) {
+        let endDate = Date()
+        let startDate = Calendar.current.date(byAdding: .month, value: -1, to: endDate) ?? Date()
+        
+        return (endDate, startDate)
     }
 }
